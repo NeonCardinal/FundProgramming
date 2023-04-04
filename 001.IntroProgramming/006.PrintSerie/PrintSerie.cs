a3a38651560ffe6fa45eacd2114a70e09fd2fd9a
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-04 11:07:45 +0300

    add

---
+++ 001.IntroProgramming/006.PrintSerie/PrintSerie.cs
@@ -0,0 +1,21 @@
+﻿//Chapter 001, Task 009
+//23.03.2023, 23:56
+using System;
+
+public class PrintSerie
+{
+    static void Main()
+    {
+        for(int i = 2; i <= 102; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine($"-{i}");
+            }
+        }
+    }
+}
