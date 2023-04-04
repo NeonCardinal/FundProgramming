9c8b9bae03dd31e5d85d9c206f8aee5ea3beecc0
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-04 11:07:09 +0300

    add

---
+++ 001.IntroProgramming/004.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,13 @@
+﻿//Chapter 001, Task 007
+//28.03.2023, 23:53
+using System;
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(101);
+        Console.WriteLine(1001);
+    }
+}
