ed4661b581360ac03ec48b8e7d5b79363ab55437
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:38:13 +0300

    ADD

---
+++ 002.PritiveTypesAndVariablesLab/001.DeclareSomeVariables/DeclareSomeVariables.cs
@@ -0,0 +1,21 @@
+﻿//Chapter 002, Task 001
+//01.04.2023, 21:15
+using System;
+
+public class DeclareSomeVariables
+{
+    static void Main()
+    {
+        byte centuries = 20;
+        ushort years = 2000;
+        uint days = 730480;
+        ulong hours = 17531520;
+
+        Console.WriteLine(centuries + " centuries are " + years + " years, or " +
+            days + " days, or " + hours + " hours.");
+
+        ulong maxValue = ulong.MaxValue;
+
+        Console.WriteLine(maxValue);
+    }
+}
