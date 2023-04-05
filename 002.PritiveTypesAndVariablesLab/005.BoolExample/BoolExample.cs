ade56c7a4d149f021a29202ce5ade70f0d27b227
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:39:28 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/005.BoolExample/BoolExample.cs
@@ -0,0 +1,27 @@
+﻿//Chapter 002, Task 005;
+//02.04.2023, 21:35
+using System;
+
+public class BoolExample
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+
+        bool greaterAB = (a > b);
+        bool equalA1 = a == 1;
+
+        if(greaterAB)
+        {
+            Console.WriteLine("A > B");
+        }
+        else
+        {
+            Console.WriteLine("B > A");
+        }
+
+        Console.WriteLine($"greaterAB = {greaterAB}");
+        Console.WriteLine($"equalA1 = {equalA1}");
+    }
+}
