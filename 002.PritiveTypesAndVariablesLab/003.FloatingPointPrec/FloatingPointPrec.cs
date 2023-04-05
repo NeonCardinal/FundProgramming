f7add624c3a5583bb7a772f9c5613730e7935ce2
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:38:50 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/003.FloatingPointPrec/FloatingPointPrec.cs
@@ -0,0 +1,15 @@
+﻿//Chapter 002, Task 003
+//02.04.2023, 21:26
+using System;
+
+public class FloatingPointPrec
+{
+    static void Main()
+    {
+        float floatPI = 3.141592653589793238f;
+        double doublePI = 3.141592653589793238;
+
+        Console.WriteLine("Float PI is: " + floatPI);
+        Console.WriteLine("Double PI is: " + doublePI);
+    }
+}
