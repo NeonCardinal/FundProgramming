deee1cfa819f05347d2139fea96edacbdf489fbc
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:39:46 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/006.CharExample/CharExample.cs
@@ -0,0 +1,21 @@
+﻿//Chapter 002, Task 006
+//02.04.2023, 22:33
+using System;
+
+public class CharExample
+{
+    static void Main()
+    {
+        char symbol = 'a';
+
+        Console.WriteLine($"The code of '{symbol}' is {(int)symbol}");
+
+        symbol = 'b';
+
+        Console.WriteLine($"The code of '{symbol}' is {(int)symbol}");
+
+        symbol = 'A';
+
+        Console.WriteLine($"The code of '{symbol}' is {(int)symbol}");
+    }
+}
