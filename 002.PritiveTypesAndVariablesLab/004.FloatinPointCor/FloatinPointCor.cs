05010ef83cfe7cdfc39467b11e6c9bd3f7782037
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:39:09 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/004.FloatinPointCor/FloatinPointCor.cs
@@ -0,0 +1,21 @@
+﻿//Chapter 002, Task 004
+//01.04.2023, 21:30
+using System;
+
+public class FloatinPointCor
+{
+    static void Main()
+    {
+        float f = 0.1f;
+        Console.WriteLine(f);
+
+        double d = 0.1;
+        Console.WriteLine(d);
+
+        float ff = 1.0f / 3;
+        Console.WriteLine(ff);
+
+        double dd = ff;
+        Console.WriteLine(dd);
+    }
+}
