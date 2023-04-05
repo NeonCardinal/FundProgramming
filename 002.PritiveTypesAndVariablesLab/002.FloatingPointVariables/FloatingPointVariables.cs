7dd82f2048aec28bce20e255f9d9f41fe5a19c52
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:38:31 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/002.FloatingPointVariables/FloatingPointVariables.cs
@@ -0,0 +1,24 @@
+﻿//Chapter 002, Task 002
+//01.04.2023, 21:21
+using System;
+
+public class FloatingPointVariables
+{
+    static void Main()
+    {
+        float floatPI = 3.14F;
+        Console.WriteLine(floatPI);
+
+        double doublePI = 3.14;
+        Console.WriteLine(doublePI);
+
+        double num = 1.56e+12;
+        Console.WriteLine(num);
+
+        double nan = double.NaN;
+        Console.WriteLine(nan);
+
+        double infinity = double.PositiveInfinity;
+        Console.WriteLine(infinity);
+    }
+}
