aa164791c14e483f50f43406628ae14ca838e006
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-05 10:40:04 +0300

    add

---
+++ 002.PritiveTypesAndVariablesLab/007.StringExample/StringExample.cs
@@ -0,0 +1,16 @@
+﻿//Primitive types and variables - lab, Task 007
+//03.04.2023, 17:16
+using System;
+
+public class StringExample
+{
+    static void Main()
+    {
+        string firstName = "Ivan";
+        string lastName = "Ivanov";
+        string fullName = firstName + " " + lastName;
+
+        Console.WriteLine($"Hello, {firstName}!");
+        Console.WriteLine($"Your full name is {fullName}.");
+    }
+}
