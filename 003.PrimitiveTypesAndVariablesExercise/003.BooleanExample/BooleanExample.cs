b21c453761b621cb4bbe5160520c90a08d9fe524
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:29:04 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/003.BooleanExample/BooleanExample.cs
@@ -0,0 +1,13 @@
+﻿//Primitive types and variables - exercise, Task 006
+//03.04.20223, 17:45
+using System;
+
+public class BooleanExample
+{
+    static void Main()
+    {
+        bool isMale = true;
+
+        Console.WriteLine(isMale);
+    }
+}
