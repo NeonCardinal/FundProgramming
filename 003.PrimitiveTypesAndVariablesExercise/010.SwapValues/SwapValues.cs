6ec41279ccc575024843026f8a3053db2f870908
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:31:03 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/010.SwapValues/SwapValues.cs
@@ -0,0 +1,19 @@
+﻿//Primitive types and variables - exercise, Task 013
+//03.04.2023, 18:05
+using System;
+
+public class SwapValues
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
