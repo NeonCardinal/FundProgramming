3ba3afd7a7a34fe229a7d4990dac3dd1f3f1edf6
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:28:30 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/001.HexExample/HexExample.cs
@@ -0,0 +1,13 @@
+﻿//Primitive Types and variables - exercise, Task 004
+//03.04.2023, 17:42
+using System;
+
+public class HexExample
+{
+    static void Main()
+    {
+        int hex = 0x100;
+
+        Console.WriteLine(hex);
+    }
+}
