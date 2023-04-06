5e03d4b6f2be08ea478e93d02a90ad1855c0958c
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:29:52 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/006.QuotedStr/QuotedStr.cs
@@ -0,0 +1,15 @@
+﻿//Primitive types and variables - exercise, Task 009
+//03.04.2023, 17:51
+using System;
+
+public class QuotedStr
+{
+    static void Main()
+    {
+        string str1 = "The \"use\" of quotations causes difficulties.";
+        string str2 = @"The ""use"" of quatations causes difficulties.";
+
+        Console.WriteLine(str1);
+        Console.WriteLine(str2);
+    }
+}
