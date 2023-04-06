2c526195a62407a1dad5e0c1403f9e0c544a79c9
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:28:46 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/002.CharExample/CharExample.cs
@@ -0,0 +1,13 @@
+﻿//Primitive types and variables - exercise, Task 005
+//03.04.2023, 17:44
+using System;
+
+public class CharExample
+{
+    static void Main()
+    {
+        char ch = '\u0048';
+
+        Console.WriteLine(ch);
+    }
+}
