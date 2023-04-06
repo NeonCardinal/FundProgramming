7db507bb159fd3724972282232e263d74bde0c4b
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:30:41 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/009.Firm/Firm.cs
@@ -0,0 +1,21 @@
+﻿//Primitive types and variables - exercise, Task 012
+//03.04.2023, 18:02
+using System;
+
+public class Firm
+{
+    static void Main()
+    {
+        string firstName = "Pejo";
+        string lastName = "Pejov";
+        int age = 20;
+        char gender = 'm';
+        int id = 2756999;
+
+        Console.WriteLine(firstName);
+        Console.WriteLine(lastName);
+        Console.WriteLine(age);
+        Console.WriteLine(gender);
+        Console.WriteLine(id);
+    }
+}
