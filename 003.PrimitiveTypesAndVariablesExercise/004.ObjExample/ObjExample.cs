2ea3e0d91f1e021d3ae865190394febb5b1bcbaa
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:29:19 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/004.ObjExample/ObjExample.cs
@@ -0,0 +1,15 @@
+﻿//Primitive Types and variables - exercise, Task 007
+//03.04.2023, 17:47
+using System;
+
+public class ObjExample
+{
+    static void Main()
+    {
+        string str1 = "Hello";
+        string str2 = "World";
+        object obj = str1 + " " + str2;
+
+        Console.WriteLine(obj);
+    }
+}
