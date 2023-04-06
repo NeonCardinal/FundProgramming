90dd35bce09582d1a389b268e433e565f97168a1
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:29:36 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/005.ObjAndStr/ObjAndStr.cs
@@ -0,0 +1,16 @@
+﻿//Primitive types and variables - exercise, Task 008
+//03.04.2023, 17:49
+using System;
+
+public class ObjAndStr
+{
+    static void Main()
+    {
+        string str1 = "Hello";
+        string str2 = "World";
+        object obj = str1 + " " + str2;
+        string str = (string)obj;
+
+        Console.WriteLine(str);
+    }
+}
