aa6d1a7856783ced25bdd8a5cd1ab340ae2107b2
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:30:25 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/008.Triangle/Triangle.cs
@@ -0,0 +1,15 @@
+﻿//Primitive types and variables - exercise, Task 011
+//03.04.2023, 18:00
+using System;
+
+public class Triangle
+{
+    static void Main()
+    {
+        Console.WriteLine("     c     ");
+        Console.WriteLine("    c c    ");
+        Console.WriteLine("   c   c   ");
+        Console.WriteLine("  c     c  ");
+        Console.WriteLine(" c c c c c ");
+    }
+}
