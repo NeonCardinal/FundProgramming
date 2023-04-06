bf20be7095e4487e4b18d162de4a54b816d7e66e
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-06 21:30:08 +0300

    add

---
+++ 003.PrimitiveTypesAndVariablesExercise/007.Hearth/Hearth.cs
@@ -0,0 +1,18 @@
+﻿//Primitive types and variables - exercise, Task 010
+//03.04.2023, 17:55
+using System;
+
+public class Hearth
+{
+    static void Main()
+    {
+        Console.WriteLine("  o     o");
+        Console.WriteLine(" ooo   ooo");
+        Console.WriteLine("ooooo ooooo");
+        Console.WriteLine(" ooooooooo");
+        Console.WriteLine("  ooooooo");
+        Console.WriteLine("   ooooo");
+        Console.WriteLine("    ooo");
+        Console.WriteLine("     o");
+    }
+}
