be64a2da3d57a5a8bf32daddc2fa5cd0ea6bb6df
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-08 00:38:10 +0300

    add

---
+++ 005.ConsoleInputOutput/008.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,20 @@
+﻿//Console input and output, Task 008
+//04.04.2023, 13:02
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int greater = int.MinValue;
+
+        for(int i = 0; i < 5; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            greater = Math.Max(greater, number);
+        }
+
+        Console.WriteLine(greater);
+    }
+}
