d768433f2f042847b35c83bac6aea6b462582e0d
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-07 10:20:54 +0300

    add

---
+++ 004.OperatorsAndExpressions/005.ChekBit/ChekBit.cs
@@ -0,0 +1,15 @@
+﻿//Operators and expressions, Task 004
+//03.04.2023, 19:00
+using System;
+
+public class ChekBit
+{
+    static void Main()
+    {
+        int number = 21;
+
+        bool bitThree = (number & 8) != 0;
+
+        Console.WriteLine(bitThree);
+    }
+}
