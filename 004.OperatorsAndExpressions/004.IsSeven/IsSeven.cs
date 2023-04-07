b5460be0500d5b29488db58df02130561252471f
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-07 10:20:38 +0300

    add

---
+++ 004.OperatorsAndExpressions/004.IsSeven/IsSeven.cs
@@ -0,0 +1,15 @@
+﻿//Operatiors and expressions, Task 003
+//03.04.2023, 18:54
+using System;
+
+public class IsSeven
+{
+    static void Main()
+    {
+        int number = 137;
+
+        int digit = number % 10;
+
+        Console.WriteLine(digit == 7 ? "yes" : "no");
+    }
+}
