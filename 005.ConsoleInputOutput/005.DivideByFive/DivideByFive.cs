472be7165831815a37a366b71b46c8837f3be27a
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-08 00:37:22 +0300

    add

---
+++ 005.ConsoleInputOutput/005.DivideByFive/DivideByFive.cs
@@ -0,0 +1,22 @@
+﻿//Console input and output, Task 005
+//04.04.2023, 12:52
+using System;
+
+public class DivideByFive
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        int cnt = 0;
+
+        for(int i = start; i <= end; i++)
+        {
+            if (i % 5 == 0)
+                cnt++;
+        }
+
+        Console.WriteLine(cnt);
+    }
+}
