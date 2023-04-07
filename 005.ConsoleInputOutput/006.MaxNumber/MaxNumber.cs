a1ea967133fde9301e66d22d051638c04cdad078
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-08 00:37:39 +0300

    add

---
+++ 005.ConsoleInputOutput/006.MaxNumber/MaxNumber.cs
@@ -0,0 +1,16 @@
+﻿//Console input and output, Task 006
+//04.04.2023, 12:54
+using System;
+
+public class MaxNumber
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int greater = Math.Max(a, b);
+
+        Console.WriteLine(greater);
+    }
+}
