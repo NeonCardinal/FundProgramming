54de2c66da09d99862cf00f773c0fc4ae16db5c4
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-08 00:37:54 +0300

    add

---
+++ 005.ConsoleInputOutput/007.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,24 @@
+﻿//Console input and output, Task 007
+//04.04.2023, 12:57
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int sum = 0;
+        int number;
+
+        for(int i = 0; i < 5; i++)
+        {
+            if(!(int.TryParse(Console.ReadLine(), out number)))
+            {
+                i--;
+            }
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
