a7eda9d3bfc1126c0c32ae540d7b0ba01e80a3e5
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-07 10:20:20 +0300

    add

---
+++ 004.OperatorsAndExpressions/003.DivideByFiveAndSeven/DivideByFiveAndSeven.cs
@@ -0,0 +1,15 @@
+﻿//Operators and expressions, Task 002
+//03.04.2023, 18:51
+using System;
+
+public class DivideByFiveAndSeven
+{
+    static void Main()
+    {
+        int number = 31;
+
+        bool isDivide = number % 5 == 0 && number % 7 == 0;
+
+        Console.WriteLine(isDivide ? "yes" : "no");
+    }
+}
