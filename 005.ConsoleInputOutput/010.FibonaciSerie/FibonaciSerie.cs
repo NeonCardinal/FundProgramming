4ca540094e6f5b15895a53fae4ea2be2b4dc61ae
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-08 00:38:43 +0300

    add

---
+++ 005.ConsoleInputOutput/010.FibonaciSerie/FibonaciSerie.cs
@@ -0,0 +1,24 @@
+﻿//Console input and output, Task 010
+//04.04.2023, 13:07
+using System;
+
+public class FibonaciSerie
+{
+    static void Main()
+    {
+        long first = 0;
+        long second = 1;
+
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+
+        for(int i = 1; i <= 100; i++)
+        {
+            long fib = first + second;
+            first = second;
+            second = fib;
+
+            Console.WriteLine(fib);
+        }
+    }
+}
