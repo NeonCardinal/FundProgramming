cef1e789e171fdceb3769d4ae1527075ee9d5381
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-10 11:33:44 +0300

    add

---
+++ 006.ConditionalStatements/006.SortNumbers/SortNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Conditional statements, Task 007
+//07.04.2023, 21:32
+using System;
+
+public class SortNumbers
+{
+    static void Main()
+    {
+        int greater = int.MinValue;
+
+        for(int i = 0; i < 5; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > greater)
+            {
+                greater = number;
+            }
+        }
+
+        Console.WriteLine(greater);
+    }
+}
