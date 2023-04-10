c28a88c46e0a78f16cc9e9b6f9bbdc04c344c360
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-10 11:32:33 +0300

    add

---
+++ 006.ConditionalStatements/002.SwapValues/SwapValues.cs
@@ -0,0 +1,22 @@
+﻿//Conditional statements, Task 001
+//04.04.2023, 14:37
+using System;
+
+public class SwapValues
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        if(a > b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+}
