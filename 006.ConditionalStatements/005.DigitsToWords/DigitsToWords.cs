b9cd8b340557b8010ac8315c02d8f0a2bb86a6ea
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-10 11:33:27 +0300

    add

---
+++ 006.ConditionalStatements/005.DigitsToWords/DigitsToWords.cs
@@ -0,0 +1,48 @@
+﻿//Conditional statements, Task 005
+//04.04.2023, 14:45
+using System;
+
+public class DigitsToWords
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        switch(number)
+        {
+            case 0:
+                Console.WriteLine("zero");
+                break;
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            case 4:
+                Console.WriteLine("four");
+                break;
+            case 5:
+                Console.WriteLine("five");
+                break;
+            case 6:
+                Console.WriteLine("six");
+                break;
+            case 7:
+                Console.WriteLine("seven");
+                break;
+            case 8:
+                Console.WriteLine("eight");
+                break;
+            case 9:
+                Console.WriteLine("nine");
+                break;
+            default:
+                Console.WriteLine("Error!");
+                break;
+        }
+    }
+}
