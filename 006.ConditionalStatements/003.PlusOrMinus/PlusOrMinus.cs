3ef3e7d42b61ab2b0e3bfa8c6bfc3d53ed384fe4
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-10 11:32:51 +0300

    add

---
+++ 006.ConditionalStatements/003.PlusOrMinus/PlusOrMinus.cs
@@ -0,0 +1,49 @@
+﻿//Conditional statements, Task 002
+//04.04.2023, 14:40
+using System;
+
+public class PlusOrMinus
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int cnt = 0;
+
+        if (a < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+        if (b < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+        if (c < 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+
+        if (cnt % 2 == 0)
+        {
+            Console.WriteLine("+");
+        }
+        else
+        {
+            Console.WriteLine("-");
+        }
+    }
+}
