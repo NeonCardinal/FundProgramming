3a585df8c095d31253455b66a40276a159b39e05
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-10 11:34:04 +0300

    add

---
+++ 006.ConditionalStatements/007.PlayWithSwitch/PlayWithSwitch.cs
@@ -0,0 +1,39 @@
+﻿//Conditional statements, Task 008
+//07.04.2023, 21:35
+using System;
+
+public class PlayWithSwitch
+{
+    static void Main()
+    {
+        string cmd = Console.ReadLine();
+
+        switch(cmd)
+        {
+            case "int":
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    number += 1;
+                    Console.WriteLine(number);
+                }
+                break;
+            case "double":
+                {
+                    double number = double.Parse(Console.ReadLine());
+                    number += 1;
+                    Console.WriteLine(number);
+                }
+                break;
+            case "string":
+                {
+                    string str = Console.ReadLine();
+                    str += "*";
+                    Console.WriteLine(str);
+                }
+                break;
+            default:
+                Console.WriteLine("Wrong input!");
+                break;
+        }
+    }
+}
