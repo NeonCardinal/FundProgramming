017705829b8362bfd10f1280cf160447ff8c773a
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:38:36 +0300

    add

---
+++ 007.Loops/003.PrimeNumberWhile/PrimeNumberWhile.cs
@@ -0,0 +1,28 @@
+﻿//Loops, Task 003
+//07.04.2023, 22:26
+using System;
+
+public class PrimeNumberWhile
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive number: ");
+
+        int number = int.Parse(Console.ReadLine());
+        int divider = 2;
+        int maxDivider = (int)Math.Sqrt(number);
+        bool prime = true;
+
+        while(prime && (divider <= maxDivider))
+        {
+            if(number % divider == 0)
+            {
+                prime = false;
+            }
+
+            divider++;
+        }
+
+        Console.WriteLine($"Prime? {prime}");
+    }
+}
