b1c5c5c124063c81ffd2627c614a8951bf7f08e7
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:40:35 +0300

    add

---
+++ 007.Loops/010.HappyNumbers/HappyNumbers.cs
@@ -0,0 +1,26 @@
+﻿//Loops, Task 010
+//08.04.2023, 22:16
+using System;
+
+public class HappyNumbers
+{
+    static void Main()
+    {
+        for(int a = 1; a <= 9; a++)
+        {
+            for(int b = 0; b <= 9; b++)
+            {
+                for(int c = 0; c <= 9; c++)
+                {
+                    for(int d = 0; c <= 9; d++)
+                    {
+                        if((a + b) == (c + d))
+                        {
+                            Console.WriteLine($" {a} {b} {c} {d} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
