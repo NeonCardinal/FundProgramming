0393ae4772c88d88e52d20218863a693494cf48f
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:39:57 +0300

    add

---
+++ 007.Loops/008.PrintTriangle/PrintTriangle.cs
@@ -0,0 +1,21 @@
+﻿//Loops, Task 008
+//08.04.2023, 21:15
+using System;
+
+public class PrintTriangle
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write($"{j} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
