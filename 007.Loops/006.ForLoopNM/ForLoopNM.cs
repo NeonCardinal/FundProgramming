0f4744f3be37964dc20757e1309de7f266915295
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:39:38 +0300

    add

---
+++ 007.Loops/006.ForLoopNM/ForLoopNM.cs
@@ -0,0 +1,23 @@
+﻿//Loops, Task 006
+//08.04.2023, 21:07
+using System;
+
+public class ForLoopNM
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("m = ");
+        int m = int.Parse(Console.ReadLine());
+
+        decimal result = 1;
+
+        for(int i = 0; i < m; i++)
+        {
+            result *= n;
+        }
+
+        Console.WriteLine($"n ^ m = {result}");
+    }
+}
