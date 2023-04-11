50f4b2d3095aa8c6b67a46218d8ead582064701f
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:39:19 +0300

    add

---
+++ 007.Loops/005.FactorielDoWhile/FactorielDoWhile.cs
@@ -0,0 +1,24 @@
+﻿//Loops, Task 005
+//07.04.2023, 22:36
+using System;
+
+public class FactorielDoWhile
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+
+        int n = int.Parse(Console.ReadLine());
+
+        decimal factoriel = 1;
+
+        do
+        {
+            factoriel *= n;
+            n--;
+        }
+        while (n >= 1);
+
+        Console.WriteLine($"n! = {factoriel}");
+    }
+}
