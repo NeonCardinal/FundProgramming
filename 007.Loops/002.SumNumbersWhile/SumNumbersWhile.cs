5fb076a836b100d6be6af511c9f87b28a3b79a05
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:38:19 +0300

    add

---
+++ 007.Loops/002.SumNumbersWhile/SumNumbersWhile.cs
@@ -0,0 +1,27 @@
+﻿//Loops, Task 002
+//07.04.2023, 22:21
+using System;
+
+public class SumNumbersWhile
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        int num = 1;
+        int sum = 1;
+
+        Console.Write("The sum 1");
+
+        while (num < n)
+        {
+            num++;
+            sum += num;
+
+            Console.Write(" + " + num);
+        }
+
+        Console.WriteLine(" = " + sum);
+    }
+}
