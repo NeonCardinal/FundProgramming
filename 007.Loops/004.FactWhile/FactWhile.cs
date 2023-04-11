1a230a19f7a9c21f7600f855d65074074dc586b4
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:39:02 +0300

    add

---
+++ 007.Loops/004.FactWhile/FactWhile.cs
@@ -0,0 +1,26 @@
+﻿//Loops, Task 004
+//07.04.2023, 22:32
+using System;
+
+public class FactWhile
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        decimal factoriel = 1;
+
+        while(true)
+        {
+            if(n <= 1)
+            {
+                break;
+            }
+
+            factoriel *= n;
+            n--;
+        }
+
+        Console.WriteLine($"n! = {factoriel}");
+    }
+}
