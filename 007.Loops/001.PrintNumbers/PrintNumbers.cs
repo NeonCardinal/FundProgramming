f3ae21c5e10dc02378250058a39d9b95d3117a62
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:38:02 +0300

    add

---
+++ 007.Loops/001.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,18 @@
+﻿//Loops, Task 001
+//07.04.2023, 22:20
+using System;
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        int counter = 5;
+
+        while(counter <= 10)
+        {
+            Console.WriteLine($"Number: {counter}");
+
+            counter++;
+        }
+    }
+}
