e29fb38fdfb63a6546fe73c7641f9883aabf1612
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:40:53 +0300

    add

---
+++ 007.Loops/011.PrintNumbersForLoop/PrintNumbersForLoop.cs
@@ -0,0 +1,16 @@
+﻿//Loops, Task 011
+//08.04.2023, 22:26
+using System;
+
+public class PrintNumbersForLoop
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
