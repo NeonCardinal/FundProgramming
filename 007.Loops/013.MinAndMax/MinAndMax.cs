cf9d234334b4ccf28300f397bafe818f877a0138
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:41:30 +0300

    add

---
+++ 007.Loops/013.MinAndMax/MinAndMax.cs
@@ -0,0 +1,30 @@
+﻿//Loops, Task 013
+//08.04.2023, 22:31
+using System;
+
+public class MinAndMax
+{
+    static void Main()
+    {
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        for(int i = 0; i < 10; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number < min)
+            {
+                min = number;
+            }
+
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"max = {max}");
+        Console.WriteLine($"min = {min}");
+    }
+}
