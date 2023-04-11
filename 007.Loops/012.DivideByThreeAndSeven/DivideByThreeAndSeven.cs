38ec54f018c63d4e95137a2090d2aea58a85b2f2
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-11 09:41:11 +0300

    add

---
+++ 007.Loops/012.DivideByThreeAndSeven/DivideByThreeAndSeven.cs
@@ -0,0 +1,19 @@
+﻿//Loops, Task 012
+//08.04.2023, 22:28
+using System;
+
+public class DivideByThreeAndSeven
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            if((i % 3 != 0) && (i % 7 != 0))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
