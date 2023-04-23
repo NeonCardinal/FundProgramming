fc5b44f928a45cacd52fabc9d2692094bd147080
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-23 23:03:04 +0300

    add

---
+++ 008.Arrays/002.SymetricArrays/SymetricArrays.cs
@@ -0,0 +1,32 @@
+﻿//Arrays, Task 002
+//11.04.2023, 23:31
+using System;
+
+public class SymetricArrays
+{
+    static void Main()
+    {
+        Console.Write("Enter positive integer: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        bool symetric = true;
+
+        for(int i = 0; i < array.Length / 2; i++)
+        {
+            if(array[i] != array[n - i - 1])
+            {
+                symetric = false;
+                break;
+            }
+        }
+
+        Console.WriteLine($"Is symetric? {symetric}");
+    }
+}
