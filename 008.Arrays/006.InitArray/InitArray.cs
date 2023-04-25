6c2c6b296ace12ea46dfb82d99b41f975d71a431
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-25 23:13:12 +0300

    add

---
+++ 008.Arrays/006.InitArray/InitArray.cs
@@ -0,0 +1,18 @@
+﻿//Arrays, Task 006
+//25.04.2023, 21:05
+using System;
+
+public class InitArray
+{
+    static void Main()
+    {
+        int[] arr = new int[20];
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = i * 5;
+        }
+
+        Console.WriteLine(String.Join(" ", arr));
+    }
+}
