3a7861f967da7c82828ec792a55bc4dcac78fb81
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-25 23:13:32 +0300

    add

---
+++ 008.Arrays/007.EqualArrays/EqualArrays.cs
@@ -0,0 +1,58 @@
+﻿//Arrays, Task 007
+//25.04.2023, 21:38
+using System;
+
+public class EqualArrays
+{
+    static void Main()
+    {
+        Console.Write("Vuvedete stoinost za n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Vuvedete stoinost za m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        int[] firstArray = new int[n];
+        int[] secondArray = new int[m];
+
+        for(int i = 0; i < n; i++)
+        {
+            firstArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        for(int i = 0; i < m; i++)
+        {
+            secondArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        bool isEqual = true;
+
+        if(n != m)
+        {
+            isEqual = false;
+        }
+        else
+        {
+            for(int i = 0; i < n; i++)
+            {
+                if(firstArray[i] != secondArray[i])
+                {
+                    isEqual = false;
+                    break;
+                }
+                else
+                {
+                    isEqual = true;
+                }
+            }
+        }
+
+        if(isEqual)
+        {
+            Console.WriteLine("Ednakvi sa.");
+        }
+        else
+        {
+            Console.WriteLine("Ne sa ednakvi");
+        }
+    }
+}
