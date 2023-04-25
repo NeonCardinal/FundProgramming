18f34809d19bdb1f5ed97af4e49d98f0800e953a
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-25 23:12:19 +0300

    add

---
+++ 008.Arrays/003.ReadMatrix/ReadMatrix.cs
@@ -0,0 +1,36 @@
+﻿//Arrays, Task 003
+//24.04.2023, 24:42
+using System;
+
+public class ReadMatrix
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of the rows: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of the cols: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        for(int row = 0; row < rows; row++)
+        {
+            for(int col = 0; col < cols; col++)
+            {
+                Console.Write($"matrix[{row},{col}] = ");
+                matrix[row, col] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($" {matrix[i, j]}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
