5667036c3765711e6263c09cdc35ae00b8e4c946
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-28 22:02:02 +0300

    add

---
+++ 009.Methods/002.PrintSign/PrintSign.cs
@@ -0,0 +1,30 @@
+﻿//Methods, Example 002
+//26.04.2023, 00:51
+using System;
+
+public class PrintSign
+{
+    static void Main()
+    {
+        Console.Write("Vyvedete cqlo chislo: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PSign(n);
+    }
+
+    static void PSign(int number)
+    {
+        if(number > 0)
+        {
+            Console.WriteLine("Positive");
+        }
+        else if(number < 0)
+        {
+            Console.WriteLine("Negative");
+        }
+        else
+        {
+            Console.WriteLine("Zero");
+        }
+    }
+}
