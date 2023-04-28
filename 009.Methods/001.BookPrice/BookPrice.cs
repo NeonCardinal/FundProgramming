2ca981ab906148d64fdcffda5b666f0ad02b46e7
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-28 22:01:45 +0300

    add

---
+++ 009.Methods/001.BookPrice/BookPrice.cs
@@ -0,0 +1,25 @@
+﻿//Methods, Example 001
+//26.04.2023, 00:48
+using System;
+
+public class BookPrice
+{
+    static void Main()
+    {
+        decimal[] prices = { 12.01M, 1.19M, 3.14M };
+
+        PrintTotalAmountForBooks(prices);
+    }
+
+    static void PrintTotalAmountForBooks(decimal[] prices)
+    {
+        decimal totalAmount = 0;
+
+        foreach(decimal price in prices)
+        {
+            totalAmount += price;
+        }
+
+        Console.WriteLine($"The total amoung of all book is : {totalAmount:F2}");
+    }
+}
