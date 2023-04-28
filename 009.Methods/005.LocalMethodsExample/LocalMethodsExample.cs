501ac32201ffe853e0570aa892874a44fa06ecec
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-28 22:02:53 +0300

    add

---
+++ 009.Methods/005.LocalMethodsExample/LocalMethodsExample.cs
@@ -0,0 +1,15 @@
+﻿//Methods, Example 005
+//28.04.2023, 21:14
+using System;
+
+public class LocalMethodsExample
+{
+    static void Main()
+    {
+        int age = 17;
+
+        void PrintAgeAfter(int years) => Console.WriteLine(age + years);
+
+        PrintAgeAfter(3);
+    }
+}
