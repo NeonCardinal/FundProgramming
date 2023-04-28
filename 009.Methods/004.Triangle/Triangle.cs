df43c0e824edc4d4f59437a8360a45f8f85addf0
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-28 22:02:35 +0300

    add

---
+++ 009.Methods/004.Triangle/Triangle.cs
@@ -0,0 +1,33 @@
+﻿//Methods, Example 004
+//28.04.2023, 20:02
+using System;
+
+public class Triangle
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        for(int i = 1; i <= n; i++)
+        {
+            PrintLine(1, i);
+        }
+
+        for(int i = n - 1; i >= 1; i--)
+        {
+            PrintLine(1, i);
+        }
+    }
+
+    static void PrintLine(int start, int end)
+    {
+        for(int i = start; i <= end; i++)
+        {
+            Console.Write(" " + i);
+        }
+
+        Console.WriteLine();
+    }
+}
