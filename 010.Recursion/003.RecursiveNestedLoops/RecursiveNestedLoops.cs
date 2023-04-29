8a484c09f4b24816bc06c2c69bafc1beafeccc3e
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-29 12:53:05 +0300

    add

---
+++ 010.Recursion/003.RecursiveNestedLoops/RecursiveNestedLoops.cs
@@ -0,0 +1,48 @@
+﻿//Recursion, Example 003
+//28.04.2023, 23:41
+using System;
+
+public class RecursiveNestedLoops
+{
+    static int numberOfLoops;
+    static int numberOfIterations;
+    static int[] loops;
+
+    static void Main()
+    {
+        Console.Write("N = ");
+        numberOfLoops = int.Parse(Console.ReadLine());
+
+        Console.Write("K = ");
+        numberOfIterations = int.Parse(Console.ReadLine());
+
+        loops = new int[numberOfLoops];
+
+        NestedLoops(0);
+    }
+
+    static void NestedLoops(int currentLoop)
+    {
+        if(currentLoop == numberOfLoops)
+        {
+            PrintLoops();
+            return;
+        }
+
+        for(int i = 1; i <= numberOfIterations; i++)
+        {
+            loops[currentLoop] = i;
+            NestedLoops(currentLoop + 1);
+        }
+    }
+
+    static void PrintLoops()
+    {
+        for(int i = 0; i < numberOfLoops; i++)
+        {
+            Console.Write($"{loops[i]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
