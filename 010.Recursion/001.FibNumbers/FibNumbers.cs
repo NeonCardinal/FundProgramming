623ba2519bab7569f3d5789312cce2cb8a49db50
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-29 12:52:29 +0300

    add

---
+++ 010.Recursion/001.FibNumbers/FibNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Recursion, Example 001
+//28.04.2023, 23:26
+using System;
+
+public class FibNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Fib(n));
+    }
+
+    static long Fib(int n)
+    {
+        if(n <= 2)
+        {
+            return 1;
+        }
+
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
