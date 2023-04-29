7b3d4463c1268155cf7bb28a47cef5d78b9f38d4
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-29 12:47:32 +0300

    add

---
+++ 009.Methods/010.HelloName/HelloName.cs
@@ -0,0 +1,19 @@
+﻿//Methods, Task 001
+//28.04.2023, 22:21
+using System;
+
+public class HelloName
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Greetings(name);
+    }
+
+    static void Greetings(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
