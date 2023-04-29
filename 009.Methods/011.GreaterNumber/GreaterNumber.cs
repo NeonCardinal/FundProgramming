7a6d7cac457812c19650ba6cf33c12074901122e
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-29 12:47:48 +0300

    add

---
+++ 009.Methods/011.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,33 @@
+﻿//Methods, Task 002
+//28.04.2023, 22:24
+using System;
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int first = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int second = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The greater number is: {GetMax(first, second)}");
+    }
+
+    static int GetMax(int first, int second)
+    {
+        int greater = 0;
+
+        if(first > second)
+        {
+            greater = first;
+        }
+        else
+        {
+            greater = second;
+        }
+
+        return greater;
+    }
+}
