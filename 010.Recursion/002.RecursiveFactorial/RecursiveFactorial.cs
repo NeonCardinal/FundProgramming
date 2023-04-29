961c72240d5b07cd97ad1e4a72a63be82d132000
NeonCardinal <nvnnaidenov@gmail.com>
2023-04-29 12:52:46 +0300

    add

---
+++ 010.Recursion/002.RecursiveFactorial/RecursiveFactorial.cs
@@ -0,0 +1,28 @@
+﻿//Recursion, Example 002
+//28.04.2023, 23:33
+using System;
+
+public class RecursiveFactorial
+{
+    static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        decimal factorial = Factorial(n);
+
+        Console.WriteLine($"{n}! = {factorial}");
+    }
+
+    static decimal Factorial(int n)
+    {
+        if(n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
