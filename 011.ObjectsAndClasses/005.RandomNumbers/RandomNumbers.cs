4e61a463b86a5e591c531f0e45f6c17ace87405a
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:05:08 +0300

    add

---
+++ 011.ObjectsAndClasses/005.RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Objects and classes, Task 002
+//30.04.2023, 14:50
+using System;
+
+public class RandomNumbers
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(rnd.Next(100, 201));
+        }
+    }
+}
