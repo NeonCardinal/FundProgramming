9121d4c7bb12c6e5194049294a855a64d12a5f70
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:06:04 +0300

    add

---
+++ 011.ObjectsAndClasses/008.CalculateSum/CalculateSum.cs
@@ -0,0 +1,22 @@
+﻿//Objects and classes, Task 010
+//30.04.2023, 15:00
+using System;
+using System.Linq;
+
+public class CalculateSum
+{
+    static void Main()
+    {
+        string str = "43 68 9 23 318";
+        int[] arr = str.Split().Select(int.Parse).ToArray();
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
