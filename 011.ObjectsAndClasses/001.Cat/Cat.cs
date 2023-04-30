23c6221be58bd1ad61d69ef2f33251a715e7523a
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:03:36 +0300

    add

---
+++ 011.ObjectsAndClasses/001.Cat/Cat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _001.Cat
+{
+    public class Cat
+    {
+        private string name;
+        private string color;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        public Cat()
+        {
+            this.name = null;
+            this.color = null;
+        }
+
+        public Cat(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+
+        public void SayMeow()
+        {
+            Console.WriteLine($"Cat {name} said: Meoooowww!");
+        }
+    }
+}
---
+++ 011.ObjectsAndClasses/001.Cat/StartUp.cs
@@ -0,0 +1,23 @@
+﻿//Objects and classes, Exaple 001
+//30.04.2023, 12:56
+using System;
+
+namespace _001.Cat
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Cat firstCat = new Cat();
+            firstCat.Name = "Tony";
+            firstCat.SayMeow();
+
+            Cat secondCat = new Cat("Pepy", "Red");
+            secondCat.SayMeow();
+            Console.WriteLine($"Cat {secondCat.Name} is {secondCat.Color}");
+
+            Cat thirdCat = new Cat();
+            thirdCat.SayMeow();
+        }
+    }
+}
