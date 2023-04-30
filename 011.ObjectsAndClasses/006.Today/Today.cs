25c0601def227aaffa1bc3b4bd017900d8094486
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:05:26 +0300

    add

---
+++ 011.ObjectsAndClasses/006.Today/Today.cs
@@ -0,0 +1,11 @@
+﻿//Objects and classes, Task 003
+//30.04.2023, 14:52
+using System;
+
+public class Today
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Today.DayOfWeek);
+    }
+}
