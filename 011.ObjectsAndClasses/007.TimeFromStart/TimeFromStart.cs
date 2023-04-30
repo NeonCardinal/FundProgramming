e32e1af99bfb512d206441af7e11bac4f520d371
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:05:45 +0300

    add

---
+++ 011.ObjectsAndClasses/007.TimeFromStart/TimeFromStart.cs
@@ -0,0 +1,18 @@
+﻿//Objects and classes, Task 004
+//30.04.2023, 14:54
+using System;
+
+public class TimeFromStart
+{
+    static void Main()
+    {
+        long time = Environment.TickCount;
+
+        long seconds = time / 1000;
+        int minutes = (int)seconds / 60;
+        int hours = minutes / 60;
+        int days = hours / 24;
+
+        Console.WriteLine($"Time from start: {days} days, {hours} hours, {minutes} minutes");
+    }
+}
