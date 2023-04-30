4443f37375564516c30f419d9440d1ddd8fd9a9e
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:04:11 +0300

    add

---
+++ 011.ObjectsAndClasses/002.SystemTest/SystemTest.cs
@@ -0,0 +1,21 @@
+﻿//Objects and classes, Example 002
+//30.04.2023, 13:27
+using System;
+
+public class SystemTest
+{
+    static void Main()
+    {
+        int sum = 0;
+        int startTime = Environment.TickCount;
+
+        for(int i = 0; i < 10000000; i++)
+        {
+            sum++;
+        }
+
+        int endTime = Environment.TickCount;
+
+        Console.WriteLine($"The time elapsed is {(endTime - startTime) / 1000.0}");
+    }
+}
