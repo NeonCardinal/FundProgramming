49673e4f1488f53ff40a4577373a111eaf94661f
nvnnaidenov <nvnnaidenov@gmail.com>
2023-04-30 15:04:50 +0300

    add

---
+++ 011.ObjectsAndClasses/004.LeapYear/LeapYear.cs
@@ -0,0 +1,13 @@
+﻿//Objects and classes, Task 001
+//30.04.2023, 14:48
+using System;
+
+public class LeapYear
+{
+    static void Main()
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(DateTime.IsLeapYear(year));
+    }
+}
