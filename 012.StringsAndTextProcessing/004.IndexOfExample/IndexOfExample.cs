a75ef3eaffa52bb931f59fcb6dccb6ef7c44ad0a
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:44:00 +0300

    add

---
+++ 012.StringsAndTextProcessing/004.IndexOfExample/IndexOfExample.cs
@@ -0,0 +1,20 @@
+﻿//Strings and text processing, Example 004
+//01.05.2023, 19:37
+using System;
+
+public class IndexOfExample
+{
+    static void Main()
+    {
+        string quote = "The main intent of the \"Intro C#\"" +
+            " book is to introduce the C# programming to newbies.";
+        string keyword = "C#";
+        int index = quote.IndexOf(keyword);
+
+        while(index != -1)
+        {
+            Console.WriteLine($"{keyword} found at index: {index}");
+            index = quote.IndexOf(keyword, index + keyword.Length);
+        }
+    }
+}
