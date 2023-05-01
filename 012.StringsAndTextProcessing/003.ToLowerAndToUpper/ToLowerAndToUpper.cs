459dfa02821e468cc2521cc6e71ac115b70cc30c
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:43:40 +0300

    add

---
+++ 012.StringsAndTextProcessing/003.ToLowerAndToUpper/ToLowerAndToUpper.cs
@@ -0,0 +1,21 @@
+﻿//Strings and text processing, Example 003
+//01.05.2023, 19:30
+using System;
+
+public class ToLowerAndToUpper
+{
+    static void Main()
+    {
+        string text = "All Kind OF LeTTers";
+
+        Console.WriteLine(text.ToLower());
+
+        string pass1 = "Parola";
+        string pass2 = "PaRola";
+        string pass3 = "parola";
+
+        Console.WriteLine(pass1.ToUpper() == "PAROLA");
+        Console.WriteLine(pass2.ToUpper() == "PAROLA");
+        Console.WriteLine(pass3.ToUpper() == "PAROLA");
+    }
+}
