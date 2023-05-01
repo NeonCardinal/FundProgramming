cd57811ada2d1ca95d7ee6035ac8b16a985d1f86
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:42:55 +0300

    add

---
+++ 012.StringsAndTextProcessing/001.ExapleOne/ExapleOne.cs
@@ -0,0 +1,19 @@
+﻿//Strings and text processing, Exaple 001
+//30.04.2023, 18:52
+using System;
+
+public class ExapleOne
+{
+    static void Main()
+    {
+        string message = "This is a simple string message.";
+
+        Console.WriteLine($"message = {message}");
+        Console.WriteLine($"message.Length = {message.Length}");
+
+        for(int i = 0; i < message.Length; i++)
+        {
+            Console.WriteLine($"message[{i}] = {message[i]}");
+        }
+    }
+}
