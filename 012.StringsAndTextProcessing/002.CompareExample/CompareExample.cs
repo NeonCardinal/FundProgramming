21f8d7d76f98233e676fa430b778b353b68bf800
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:43:17 +0300

    add

---
+++ 012.StringsAndTextProcessing/002.CompareExample/CompareExample.cs
@@ -0,0 +1,16 @@
+﻿//Strings and text processing, Example 002
+//01.05.2023, 00:40
+using System;
+
+public class CompareExample
+{
+    static void Main()
+    {
+        string score = "sCore";
+        string scary = "scary";
+
+        Console.WriteLine(score.CompareTo(scary));
+        Console.WriteLine(scary.CompareTo(score));
+        Console.WriteLine(scary.CompareTo(scary));
+    }
+}
