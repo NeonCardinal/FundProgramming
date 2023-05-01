fc7033dbb2bd7bdc6e3f4e45d194cf17adaafc2e
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:44:19 +0300

    add

---
+++ 012.StringsAndTextProcessing/005.SubstringExample/SubstringExample.cs
@@ -0,0 +1,15 @@
+﻿//Strings and text processing, Example 005
+//01.05.2023, 19:46
+using System;
+
+public class SubstringExample
+{
+    static void Main()
+    {
+        string path = "C:\\Pics\\Rila2017.jpg";
+        int index = path.LastIndexOf("\\");
+        string fullName = path.Substring(index + 1);
+
+        Console.WriteLine(fullName);
+    }
+}
