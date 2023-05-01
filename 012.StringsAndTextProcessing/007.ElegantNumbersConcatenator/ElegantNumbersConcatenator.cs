d17b1bcb7a2c90e85d8b07663315e4395daa0afd
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-01 23:44:59 +0300

    add

---
+++ 012.StringsAndTextProcessing/007.ElegantNumbersConcatenator/ElegantNumbersConcatenator.cs
@@ -0,0 +1,24 @@
+﻿//String and text processing, Example 007
+//01.05.2023, 20:12
+using System;
+using System.Text;
+
+public class ElegantNumbersConcatenator
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Now);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append("Numbers: ");
+
+        for(int i = 1; i <= 200000; i++)
+        {
+            sb.Append(i);
+        }
+
+        Console.WriteLine(sb.ToString().Substring(0, 1024));
+        Console.WriteLine(DateTime.Now);
+    }
+}
