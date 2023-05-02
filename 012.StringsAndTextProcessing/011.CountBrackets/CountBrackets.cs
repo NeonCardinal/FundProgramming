5c1b35042e36b4bb489fc8e63425b0cf5f099bba
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-02 17:29:03 +0300

    add

---
+++ 012.StringsAndTextProcessing/011.CountBrackets/CountBrackets.cs
@@ -0,0 +1,35 @@
+﻿//Strings and text processing, Task 003
+//02.05.2023, 17:23
+using System;
+
+public class CountBrackets
+{
+    static void Main()
+    {
+        string text = "((a + b) / 5 - d)";
+
+        int counter = 0;
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            char symbol = text[i];
+            if(symbol == '(')
+            {
+                counter++;
+            }
+            else if(symbol == ')')
+            {
+                counter--;
+            }
+        }
+
+        if(counter != 0)
+        {
+            Console.WriteLine("Incorrect");
+        }
+        else
+        {
+            Console.WriteLine("Correct");
+        }
+    }
+}
