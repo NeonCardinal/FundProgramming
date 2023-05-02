3bb9a9789dccc9d76f6dfae8f70ba11269e5f327
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-02 17:28:07 +0300

    add

---
+++ 012.StringsAndTextProcessing/008.ReverseString/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿//Strings and text processing, Example 008
+//01.05.2023, 20:15
+using System;
+using System.Text;
+
+public class ReverseString
+{
+    static void Main()
+    {
+        string text = "Neshto";
+
+        string reversed = ReverseText(text);
+
+        Console.WriteLine(reversed);
+    }
+
+    static string ReverseText(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = str.Length - 1; i >= 0; i--)
+        {
+            sb.Append(str[i]);
+        }
+
+        return sb.ToString();
+    }
+}
