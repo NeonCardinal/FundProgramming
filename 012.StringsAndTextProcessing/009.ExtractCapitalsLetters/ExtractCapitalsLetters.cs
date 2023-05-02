3ff9ec238a547bc54a6b6a839c52b78246dcc203
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-02 17:28:26 +0300

    add

---
+++ 012.StringsAndTextProcessing/009.ExtractCapitalsLetters/ExtractCapitalsLetters.cs
@@ -0,0 +1,33 @@
+﻿//Strings and text processing, Example 009
+//02.05.2023, 17:16
+using System;
+using System.Text;
+
+public class ExtractCapitalsLetters
+{
+    static void Main()
+    {
+        string str = "Hello NikI";
+
+        string result = ExtractCapitals(str);
+
+        Console.WriteLine(result);
+    }
+
+    static string ExtractCapitals(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+
+            if(char.IsUpper(ch))
+            {
+                sb.Append(ch);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
