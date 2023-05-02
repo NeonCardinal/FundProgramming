658e3d7b974937446b9617aaf25582cb39914ccf
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-02 18:03:20 +0300

    add

---
+++ 012.StringsAndTextProcessing/012.CntWordInText/CntWordInText.cs
@@ -0,0 +1,23 @@
+﻿//Strings and text processing, Task 005
+//02.05.2023, 17:43
+using System;
+
+public class CntWordInText
+{
+    static void Main()
+    {
+        string text = "We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.".ToLower();
+        string keyWord = "in";
+        int counter = 0;
+
+        int index = text.IndexOf(keyWord);
+
+        while(index != -1) 
+        {
+            counter++;
+            index = text.IndexOf(keyWord, index + 1);
+        }
+
+        Console.WriteLine(counter);
+    }
+}
