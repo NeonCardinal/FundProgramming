c4ee0bcd5c79f6eaea8a6f72740e6efffbea33fd
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-02 17:28:44 +0300

    add

---
+++ 012.StringsAndTextProcessing/010.ReverseText/ReverseText.cs
@@ -0,0 +1,20 @@
+﻿//Strings and text processing, Task 002
+//02.05.2023, 17:21
+using System;
+
+public class ReverseText
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+
+        string reversed = "";
+
+        for(int i = text.Length - 1; i >= 0; i--)
+        {
+            reversed += text[i];
+        }
+
+        Console.WriteLine(reversed);
+    }
+}
