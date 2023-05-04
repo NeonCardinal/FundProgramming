e5080d5cf6795938397f28788d609dad9d3fd505
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-04 22:52:42 +0300

    add

---
+++ 013.DefiningClasses/002.OverlappingScopeTest/OverlappingScope.cs
@@ -0,0 +1,29 @@
+﻿//Defining classes, Example 002
+//04.05.2023, 22:06
+using System;
+using System.Xml.XPath;
+
+public class OverlappingScope
+{
+    int myValue = 3;
+
+    void PrintMyValue()
+    {
+        int myValue = 5;
+
+        Console.WriteLine($"My value is: {this.myValue}");
+    }
+
+    int CalculateNewValue(int newValue)
+    {
+        int result = myValue + newValue;
+
+        return result;
+    }
+
+    static void Main()
+    {
+        OverlappingScope instance = new OverlappingScope();
+        instance.PrintMyValue();
+    }
+}
