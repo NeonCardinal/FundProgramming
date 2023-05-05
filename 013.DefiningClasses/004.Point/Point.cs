0908069e2d15a533c02778c8d4a2c9b3bd0fc162
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-05 23:22:45 +0300

    add

---
+++ 013.DefiningClasses/004.Point/Point.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+public class Point
+{
+    private double x;
+    private double y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double X
+    {
+        get { return x; }
+        set { this.x = value; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+        set { this.y = value; }
+    }
+}
---
+++ 013.DefiningClasses/004.Point/PointTest.cs
@@ -0,0 +1,17 @@
+﻿//Defining classes, Example 004
+//05.05.2023, 23:17
+using System;
+
+public class PointTest
+{
+    static void Main()
+    {
+        Point myPoint = new Point(1, 2);
+
+        double myPointXCoordinate = myPoint.X;
+        double myPointYCoordinate = myPoint.Y;
+
+        Console.WriteLine($"The X coordinate: {myPointXCoordinate}");
+        Console.WriteLine($"The Y coordinate: {myPointYCoordinate}");
+    }
+}
