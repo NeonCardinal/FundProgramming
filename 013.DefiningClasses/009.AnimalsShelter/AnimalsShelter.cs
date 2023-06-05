d2c8e971c3b66d813d1b346224f0eda9142386e9
nvnnaidenov <nvnnaidenov@gmail.com>
2023-06-05 13:37:00 +0300

    add

---
+++ 013.DefiningClasses/009.AnimalsShelter/AnimalsShelter.cs
@@ -0,0 +1,53 @@
+﻿//Defining classes, Example 009
+//05.06.2023, 12:30
+using System;
+
+public class AnimalsShelter<T>
+{
+    private const int DefaultPlacesCount = 20;
+
+    private T[] animalList;
+    private int usedPlaces;
+
+    public AnimalsShelter() : this(DefaultPlacesCount)
+    {
+
+    }
+
+    public AnimalsShelter(int placesCount)
+    {
+        this.animalList = new T[placesCount];
+        this.usedPlaces = 0;
+    }
+
+    public void Shelter(T newAnimal)
+    {
+        if(this.usedPlaces >= this.animalList.Length)
+        {
+            throw new InvalidOperationException("Shelter is full.");
+        }
+
+        this.animalList[this.usedPlaces] = newAnimal;
+        this.usedPlaces++;
+    }
+
+    public T Release(int index)
+    {
+        if (index < 0 || index >= this.usedPlaces)
+        {
+            throw new ArgumentOutOfRangeException("Invalid cell index: " + index);
+        }
+
+        T realeasedAnimal = this.animalList[index];
+
+        for (int i = index; i < this.usedPlaces - 1; i++)
+        {
+            this.animalList[i] = this.animalList[i + 1];
+        }
+
+        this.animalList[this.usedPlaces - 1] = default(T);
+        this.usedPlaces--;
+
+        return realeasedAnimal;
+    }
+}
---
+++ 013.DefiningClasses/009.AnimalsShelter/AnimalsShelterTest.cs
@@ -0,0 +1,19 @@
+﻿//Defining classes, Example 009
+//05.06.2023, 12:47
+using FirstExample;
+using System;
+
+
+public class AnimalsShelterTest
+{
+    static void Main()
+    {
+        AnimalsShelter<Dog> shelter = new AnimalsShelter<Dog>();
+
+        shelter.Shelter(new Dog());
+
+        Dog d = shelter.Release(0);
+        Console.WriteLine(d);
+        d.Bark();
+    }
+}
