a489e4468e7d3bff0b454e8b8c24102663b05cd9
nvnnaidenov <nvnnaidenov@gmail.com>
2023-06-05 15:01:09 +0300

    add

---
+++ 013.DefiningClasses/012.Library/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private string izdatelstvo;
+        private int year;
+        private string isbnNumber;
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string Izdatelstvo { get => izdatelstvo; set => izdatelstvo = value; }
+        public int Year { get => year; set => year = value; }
+        public string IsbnNumber { get => isbnNumber; set => isbnNumber = value; }
+
+
+    }
+}
---
+++ 013.DefiningClasses/012.Library/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Library
+    {
+        private string name;
+        private List<Book> books;
+
+        public string Name { get => name; set => name = value; }
+        public List<Book> Books { get => books; set => books = value; }
+
+        public Library()
+        {
+            this.name = null;
+            this.books = null;
+        }
+
+        public Library(string name, List<Book> books)
+        {
+            this.name = name;
+            this.books = books;
+        }
+
+        public void AddBook(Book book)
+        {
+            this.books.Add(book);
+        }
+
+        public Book BookSearch(Book book, string author)
+        {
+            Book result;
+
+            result = books.Find(x => x.Author == author);
+
+            return result;
+        }
+        
+        public void PrintInfoBook(Book book)
+        {
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+            Console.WriteLine(book.Year);
+            Console.WriteLine(book.IsbnNumber);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            books.Remove(book);
+        }
+    }
+}
