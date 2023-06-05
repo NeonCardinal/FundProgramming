2bfd1c21186113e6bced900dbbf0e91a83f041f4
nvnnaidenov <nvnnaidenov@gmail.com>
2023-06-05 13:37:27 +0300

    add

---
+++ 013.DefiningClasses/010.Student/StartUp.cs
@@ -0,0 +1,25 @@
+﻿//Defining classes, Task 001 - 006
+//05.06.2023, 13:09
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Student empty = new Student();
+            Student canka = new Student("Canka Petrova Petrova", 3, "Informatika", "TU");
+            Student kirilka = new Student("Kirilka Atanasova Goncheva", 1, "Informatika", "PU", "Kirilka@kiki.com", 0888123456L);
+
+            empty.StudentInfo();
+            canka.StudentInfo();
+            kirilka.StudentInfo();
+
+            Console.WriteLine($"Broi studenti: {Student.studentCounter}");
+        }
+    }
+}
---
+++ 013.DefiningClasses/010.Student/Student.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Student
+{
+    public class Student
+    {
+        private string studentNames;
+        private int kurs;
+        private string specialnost;
+        private string university;
+        private string email;
+        private long phoneNumber;
+        public static int studentCounter = 0;
+
+        public string StudentNames
+        {
+            get
+            { 
+                return studentNames; 
+            }
+            set 
+            { 
+                studentNames = value; 
+            }
+        }
+
+        public int Kurs
+        {
+            get
+            {
+                return kurs;
+            }
+            set
+            {
+                kurs = value;
+            }
+        }
+
+        public string Specialnost
+        {
+            get
+            {
+                return specialnost;
+            }
+            set
+            {
+                specialnost = value;
+            }
+        }
+
+        public string University
+        {
+            get
+            {
+                return university;
+            }
+            set
+            {
+                university = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public long PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
+        public Student()
+        {
+            this.studentNames = null;
+            this.kurs = 0;
+            this.specialnost = null;
+            this.university = null;
+            this.email = null;
+            this.phoneNumber = 0L;
+            studentCounter++;
+        }
+
+        public Student(string names, int kurs, string specialnost, string university)
+        {
+            this.studentNames = names;
+            this.kurs = kurs;
+            this.specialnost = specialnost;
+            this.university = university;
+            this.email = null;
+            this.phoneNumber = 0L;
+            studentCounter++;
+        }
+
+        public Student(string names, int kurs, string specialnost, string uni, string email, long phoneNumber)
+        {
+            this.studentNames = names;
+            this.kurs = kurs;
+            this.specialnost = specialnost;
+            this.university = uni;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            studentCounter++;
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine($"Student names: {this.studentNames}");
+            Console.WriteLine($"Student kurs: {Kurs}");
+            Console.WriteLine($"Student specialnost: {Specialnost}");
+            Console.WriteLine($"Student university: {University}");
+            Console.WriteLine($"Student email: {Email}");
+            Console.WriteLine($"Student phone number: {PhoneNumber}");
+        }
+    }
+}
