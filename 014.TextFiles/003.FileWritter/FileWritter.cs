835a9e0047f427e0be7938104dcaa050ff63fd65
NeonCardinal <nvnnaidenov@gmail.com>
2023-06-30 22:10:18 +0300

    add

---
+++ 014.TextFiles/003.FileWritter/FileWritter.cs
@@ -0,0 +1,20 @@
+﻿//Text files, Example 003
+//30.06.2023, 21:27
+using System;
+using System.IO;
+
+public class FileWritter
+{
+    static void Main()
+    {
+        StreamWriter writter = new StreamWriter("..//..//..//Test.txt");
+
+        using(writter)
+        {
+            for(int i = 1; i <= 20; i++)
+            {
+                writter.WriteLine(i);
+            }
+        }
+    }
+}
