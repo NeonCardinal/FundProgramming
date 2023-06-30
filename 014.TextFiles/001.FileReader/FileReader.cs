4ac9445813653fbea12b2cf1509baed7b9e403be
NeonCardinal <nvnnaidenov@gmail.com>
2023-06-30 22:09:34 +0300

    add

---
+++ 014.TextFiles/001.FileReader/FileReader.cs
@@ -0,0 +1,27 @@
+﻿//Text Files, Example 001
+//30.06.2023, 21:11
+using System;
+using System.IO;
+
+public class FileReader
+{
+    static void Main()
+    {
+        StreamReader reader = new StreamReader("..\\..\\..\\Texts\\SampleText.txt");
+
+        int lineNumber = 0;
+
+        string line = reader.ReadLine();
+
+        while(line != null)
+        {
+            lineNumber++;
+
+            Console.WriteLine($"Line {lineNumber}: {line}");
+
+            line = reader.ReadLine();
+        }
+
+        reader.Close();
+    }
+}
