eb041f9c1f091742acb61e8e2ae538d34aacffc9
NeonCardinal <nvnnaidenov@gmail.com>
2023-06-30 22:11:28 +0300

    add

---
+++ 014.TextFiles/006.ZeroTimes/ZeroTimes.cs
@@ -0,0 +1,25 @@
+﻿//Text files, Example 006
+//30.06.2023, 22:03
+using System;
+using System.IO;
+
+public class ZeroTimes
+{
+    static void Main()
+    {
+        string fileName = @"C:/Windows/Explorer.exe";
+        byte[] bytes = File.ReadAllBytes(fileName);
+
+        int zeroCount = 0;
+
+        foreach(byte b in bytes)
+        {
+            if(b == 0)
+            {
+                zeroCount++;
+            }
+        }
+
+        Console.WriteLine($"Zeroes: {zeroCount}");
+    }
+}
